4d7112f062520f54401fb4c041b4674bea8335f8
Sk Baloch <itxskbaloch4@gmail.com>
2026-07-08 04:10:41 +0200

    Create Footer.tsx

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="border-t border-gray-200 dark:border-gray-800 py-6 text-center text-xs text-gray-500">
+      © {new Date().getFullYear()} AgencyOS. Created by SK Baloch. All rights reserved.
+    </footer>
+  );
+}
